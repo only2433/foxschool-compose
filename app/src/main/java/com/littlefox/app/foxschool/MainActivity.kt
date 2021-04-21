e83538f2ef042fd1f20eaf21be851dd07dbb80b4
정재현 <only340@gmail.com>
2021-04-21 14:02:34 +0900

    - Gradle 라이브러리 추가

--- app/src/main/java/com/littlefox/app/foxschool/MainActivity.kt
+++ app/src/main/java/com/littlefox/app/foxschool/MainActivity.kt
@@ -1,15 +1,12 @@
-package com.littlefox.app.foxschool;
+package com.littlefox.app.foxschool
 
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.littlefox.app.foxschool.R
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
