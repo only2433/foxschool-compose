7d3499f69a3f4038c5b72a34f676df7433517fea
정재현 <only340@gmail.com>
2021-05-13 13:46:13 +0900

    - 암호화 로직 변경

--- app/src/main/java/com/littlefox/app/foxschool/enc/SimpleCrypto.kt
+++ app/src/main/java/com/littlefox/app/foxschool/enc/SimpleCrypto.kt
@@ -1,105 +1,62 @@
 package com.littlefox.app.foxschool.enc
 
-import java.security.SecureRandom
+import android.util.Base64
+import java.io.UnsupportedEncodingException
+import java.security.InvalidAlgorithmParameterException
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import java.security.spec.AlgorithmParameterSpec
+import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
+import javax.crypto.IllegalBlockSizeException
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-
-class SimpleCrypto
+object SimpleCrypto
 {
-    @Throws(Exception::class)
-    fun encrypt(cleartext : String) : String
+    var ivBytes = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+    private const val SEED = "littlefox*&70123"
+
+
+    //AES256 암호화
+    @Throws(
+        UnsupportedEncodingException::class,
+        NoSuchAlgorithmException::class,
+        NoSuchPaddingException::class,
+        InvalidKeyException::class,
+        InvalidAlgorithmParameterException::class,
+        IllegalBlockSizeException::class,
+        BadPaddingException::class
+    )
+    fun encode(str : String) : String
     {
-        val rawKey = getRawKey(SEED.toByteArray())
-        val result = encrypt(rawKey, cleartext.toByteArray())
-        return toHex(result)
+        val textBytes = str.toByteArray(charset("UTF-8"))
+        val ivSpec : AlgorithmParameterSpec = IvParameterSpec(ivBytes)
+        val newKey = SecretKeySpec(SEED.toByteArray(charset("UTF-8")), "AES")
+        var cipher : Cipher? = null
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec)
+        return Base64.encodeToString(cipher.doFinal(textBytes), 0)
     }
 
-    @Throws(Exception::class)
-    fun decrypt(encrypted : String) : String
+    //AES256 복호화
+    @Throws(
+        UnsupportedEncodingException::class,
+        NoSuchAlgorithmException::class,
+        NoSuchPaddingException::class,
+        InvalidKeyException::class,
+        InvalidAlgorithmParameterException::class,
+        IllegalBlockSizeException::class,
+        BadPaddingException::class
+    )
+    fun decode(str : String?) : String
     {
-        val rawKey = getRawKey(SEED.toByteArray())
-        val enc = toByte(encrypted)
-        val result = decrypt(rawKey, enc)
-        return String(result)
-    }
-
-    fun toHex(txt : String) : String
-    {
-        return toHex(txt.toByteArray())
-    }
-
-    fun fromHex(hex : String) : String
-    {
-        return String(toByte(hex))
-    }
-
-    fun toByte(hexString : String) : ByteArray
-    {
-        val len = hexString.length / 2
-        val result = ByteArray(len)
-        for(i in 0 until len) result[i] = Integer.valueOf(hexString.substring(2 * i, 2 * i + 2), 16).toByte()
-        return result
-    }
-
-    fun toHex(buf : ByteArray?) : String
-    {
-        if(buf == null) return ""
-        val result = StringBuffer(2 * buf.size)
-        for(i in buf.indices)
-        {
-            appendHex(result, buf[i])
-        }
-        return result.toString()
-    }
-
-    companion object
-    {
-        private const val SEED = "littlefox*&70"
-        private var sSimpleCrypto : SimpleCrypto? = null
-        fun getInstance() : SimpleCrypto?
-        {
-            if(sSimpleCrypto == null)
-            {
-                sSimpleCrypto = SimpleCrypto()
-            }
-            return sSimpleCrypto
-        }
-
-        @Throws(Exception::class)
-        private fun getRawKey(seed : ByteArray) : ByteArray
-        {
-            val kgen = KeyGenerator.getInstance("AES")
-            val sr = SecureRandom.getInstance("SHA1PRNG")
-            sr.setSeed(seed)
-            kgen.init(128, sr) // 192 and 256 bits may not be available
-            val skey = kgen.generateKey()
-            return skey.encoded
-        }
-
-        @Throws(Exception::class)
-        private fun encrypt(raw : ByteArray, clear : ByteArray) : ByteArray
-        {
-            val skeySpec = SecretKeySpec(raw, "AES")
-            val cipher = Cipher.getInstance("AES")
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec)
-            return cipher.doFinal(clear)
-        }
-
-        @Throws(Exception::class)
-        private fun decrypt(raw : ByteArray, encrypted : ByteArray) : ByteArray
-        {
-            val skeySpec = SecretKeySpec(raw, "AES")
-            val cipher = Cipher.getInstance("AES")
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec)
-            return cipher.doFinal(encrypted)
-        }
-
-        private const val HEX = "0123456789ABCDEF"
-        private fun appendHex(sb : StringBuffer, b : Byte)
-        {
-            sb.append(HEX[b.toInt() shr 4 and 0x0f]).append(HEX[b.toInt() and 0x0f])
-        }
+        val textBytes = Base64.decode(str, 0)        //byte[] textBytes = str.getBytes("UTF-8");
+        val ivSpec : AlgorithmParameterSpec = IvParameterSpec(ivBytes)
+        val newKey = SecretKeySpec(SEED.toByteArray(charset("UTF-8")), "AES")
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec)
+        return String(cipher.doFinal(textBytes), charset("UTF-8"))
     }
 }
